3a14f00c1bf2dc9993e1bc5f0e22920bfe38951c
Pujan Gautam <mepujan10@gmail.com>
2023-11-05 21:38:19 -0500

    overwrite admin site header, title, index title

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Post, Like
 
+admin.site.site_header = "Social Connect"
+admin.site.site_title = "Social Connect Admin Portal"
+admin.site.index_title = "Welcome to Social Connect  Portal"
+
 admin.site.register(Post)
 admin.site.register(Like)
