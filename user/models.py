5f0d0fb85bf474154ed2a0b4f6041fc6ed4edbe1
Pujan Gautam <mepujan10@gmail.com>
2023-11-05 23:10:15 -0500

    added Profile class and register it to admin

--- user/models.py
+++ user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
+from posts.models import BaseModel
 
-# Create your models here.
+
+class Profile(BaseModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+    quote = models.CharField(max_length=200)
+    profile_pic = models.ImageField(upload_to='profile')
+
+    def __str__(self) -> str:
+        return f'{self.user.username} profile has been created at {datetime.strptime(self.updated, "%a,%d %b, %Y %I:%M:%S %p")}.'
