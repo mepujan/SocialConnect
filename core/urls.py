a26aa46f948272b47fd3002dca60473d70f036d0
Pujan Gautam <mepujan10@gmail.com>
2023-11-06 18:32:50 -0500

    added views templete and url for homepage

--- core/urls.py
+++ core/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import homepage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', homepage, name="homepage"),
     path("posts", include('posts.urls', namespace='posts'))
 ]
 
