bb4c6f6759f28f8e9ee60e2b7df002922d388012
Pujan Gautam <mepujan10@gmail.com>
2023-11-05 22:35:20 -0500

    created base model and implemented it in post and comment class

--- comment/models.py
+++ comment/models.py
@@ -1,14 +1,12 @@
 from django.db import models
-from posts.models import Post
+from posts.models import Post, BaseModel
 from django.contrib.auth.models import User
 
 
-class Comment(models.Model):
+class Comment(BaseModel):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     comment = models.CharField(max_length=400)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.user.username} commented on post {self.pk}"
--- posts/models.py
+++ posts/models.py
@@ -2,12 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Post(models.Model):
+class BaseModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Post(BaseModel):
     status = models.TextField()
     image = models.ImageField(upload_to="posts", null=True, blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return self.user
@@ -17,12 +23,10 @@ class Post(models.Model):
         pass
 
 
-class Like(models.Model):
+class Like(BaseModel):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     is_liked = models.BooleanField(default=False)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return f'{self.user.username} liked post {self.post.pk}'
