a19adc44361314c73dd7c4a483bf87ecf7b33433
Pujan Gautam <mepujan10@gmail.com>
2023-11-08 14:11:43 -0500

    added profile template

--- core/views.py
+++ core/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from comment.form import CommentForm
+from posts.models import Post
 
 
 def homepage(request):
-    return render(request, 'homepage.html')
+    form = CommentForm()
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        post = Post.objects.get(id=request.POST.get('post'))
+        if form.is_valid():
+            instance = form.save(commit=False)
+            # print("clean-data = ", form)
+            instance.user = request.user
+            instance.post = post
+            instance.save()
+            return redirect("/posts")
+    posts = Post.objects.all()
+    return render(request, "homepage.html", {'posts': posts, 'comment_form': form})
