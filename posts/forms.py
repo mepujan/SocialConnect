e47252ffc673fb6d3318e3642a8e43f3af7d895a
Pujan Gautam <mepujan10@gmail.com>
2023-11-11 23:11:05 -0500

    update form centered

--- posts/forms.py
+++ posts/forms.py
@@ -1,8 +1,12 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Post
 
 
 class PostForm(ModelForm):
+    content = forms.CharField(
+        label="Content", widget=forms.TextInput(attrs={'rows': 4}))
+
     class Meta:
         model = Post
         fields = ('content', 'image')
