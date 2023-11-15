571f2817bb5ef562f20753e0b224bf066896089e
Pujan Gautam <mepujan10@gmail.com>
2023-11-15 16:38:26 -0500

    url for view friends and friend request

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import (profile, create_profile, login_user, signup, logout_user,
-                    ProfileDetailView, get_all_user, search_user, friend_request_received,
+                    ProfileDetailView, search_user,
                     profiles_to_add_friend, ProfileListView,
-                    send_friend_request, remove_friend, cancel_friend_request)
+                    send_friend_request, remove_friend, cancel_friend_request,
+                    ViewFriendList, FriendRequestReceivedView, accept_friend_request)
 
 
 app_name = 'profiles'
@@ -14,14 +15,16 @@ urlpatterns = [
     path('signup', signup, name='signup'),
     path("logout", logout_user, name='logout'),
     path("<int:pk>/profile", ProfileDetailView.as_view(), name="profile-detail"),
-    # path('peoples', get_all_user, name='peoples'),
     path('search', search_user, name='search-user'),
-    path('requests', friend_request_received, name='requests'),
     path('add-friend', profiles_to_add_friend, name='add-friend'),
     path('peoples', ProfileListView.as_view(), name='peoples'),
     path('add-friend/<int:user_id>', send_friend_request, name='add-friend'),
     path('remove-friend/<int:user_id>', remove_friend, name='remove-friend'),
     path('cancel-friend/<int:user_id>',
          cancel_friend_request, name='cancel-request'),
+    path('friends', ViewFriendList.as_view(), name='friends'),
+    path('requests', FriendRequestReceivedView.as_view(), name='friend-request'),
+    path('accept_request/<int:profile_id>',
+         accept_friend_request, name='accept-friend')
 
 ]
