cf84d93c62c8cf8724e6cd2440470f468fbca53c
Pujan Gautam <mepujan10@gmail.com>
2023-11-15 21:57:28 -0500

    context processor that return request received count

---
+++ user/context_processor.py
@@ -0,0 +1,9 @@
+from .models import Profile, Relationship
+from django.db.models import Q
+
+
+def request_received(request):
+    profile = Profile.objects.get(user=request.user)
+    request_available = Relationship.objects.filter(
+        Q(receiver=profile) & Q(status="send"))
+    return {'request_count': len(request_available)}
