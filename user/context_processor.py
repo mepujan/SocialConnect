a4a32f65591f6cf3074a52389458c7060d5d218e
Pujan Gautam <mepujan10@gmail.com>
2023-11-15 22:01:16 -0500

    condition added to check user is logged in or not

--- user/context_processor.py
+++ user/context_processor.py
@@ -3,7 +3,9 @@ from django.db.models import Q
 
 
 def request_received(request):
-    profile = Profile.objects.get(user=request.user)
-    request_available = Relationship.objects.filter(
-        Q(receiver=profile) & Q(status="send"))
-    return {'request_count': len(request_available)}
+    if request.user.is_authenticated:
+        profile = Profile.objects.get(user=request.user)
+        request_available = Relationship.objects.filter(
+            Q(receiver=profile) & Q(status="send"))
+        return {'request_count': len(request_available)}
+    return {}
