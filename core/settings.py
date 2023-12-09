8ebd03f52a616b5622c7557e8e583892137a6f72
Pujan Gautam <mepujan10@gmail.com>
2023-12-09 00:42:56 -0500

    updated debug flag and allowed host

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@wazu+ykvzqhwh^sjqag$x0(6xk5gjbu%vvl!^*wm6b$jkt%b5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
