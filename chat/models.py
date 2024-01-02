8b5959a4cf56fc17a46139b5d86ae1ee7ee46bc6
Pujan Gautam <mepujan10@gmail.com>
2024-01-01 20:22:03 -0500

    models to store chat msg in db

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from abstract.abstract import BaseModel
 
-# Create your models here.
+
+class ChatMessage(BaseModel):
+    sender = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='msg_sender')
+    receiver = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='msg_receiver')
+    message = models.TextField()
+
+    def __str__(self):
+        return f"{self.sender.username} send message to {self.receiver.username}"
