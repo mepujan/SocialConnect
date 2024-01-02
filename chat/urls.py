28e68247d79f8d75d34a489b335a744400e0755a
Pujan Gautam <mepujan10@gmail.com>
2024-01-01 20:23:07 -0500

    path to view msg page

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import chatPage
+from .views import chatPage, display_all_user_messages
 
 app_name = 'chat'
 
 urlpatterns = [
-    path('message/<int:id>', chatPage, name='message')
+    path('message/<int:id>', chatPage, name='message'),
+    path('messages/<int:id>/', display_all_user_messages, name='messages')
 ]
