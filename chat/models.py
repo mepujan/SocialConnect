571b4c1761cd9fbf6c7af372a314498a75a63e67
Pujan Gautam <mepujan10@gmail.com>
2024-01-01 21:02:52 -0500

    foreign key changes from user to profile

--- chat/models.py
+++ chat/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 from abstract.abstract import BaseModel
+from user.models import Profile
 
 
 class ChatMessage(BaseModel):
     sender = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='msg_sender')
+        Profile, on_delete=models.CASCADE, related_name='msg_sender')
     receiver = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='msg_receiver')
+        Profile, on_delete=models.CASCADE, related_name='msg_receiver')
     message = models.TextField()
 
     def __str__(self):
-        return f"{self.sender.username} send message to {self.receiver.username}"
+        return f"{self.sender.user.username} send message to {self.receiver.user.username}"
