adcfbceabebf9c19a2341aa0cd983e05157606c8
Pujan Gautam <mepujan10@gmail.com>
2024-01-01 20:22:36 -0500

    fetch all msg from db and render to page

--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from .models import ChatMessage
+from django.db.models import Q
 
 
 def chatPage(request, *args, **kwargs):
@@ -6,3 +8,9 @@ def chatPage(request, *args, **kwargs):
         return redirect("login-user")
     context = {}
     return render(request, "chat_page.html", context)
+
+
+def display_all_user_messages(request, id):
+    messages = ChatMessage.objects.filter(Q(sender=id) | Q(receiver=id))
+    print('messages ->', messages)
+    return render(request, 'messages.html', {'messages': messages})
